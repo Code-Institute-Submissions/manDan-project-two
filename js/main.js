a7eb6859ce92b35f97c1841c37286ba1c061b7bd
HMTRIVEDI <hirenkumar.t@yahoo.com>
2020-06-25 11:28:52 +0000

    starting shoping cart functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 console.log("working");
+
+
+//  shoping cart function methods and 
+var shoppingCart = (function() {
+  cart = [];
+  
+  // Constructor
+  function Item(name, price, count) {
+    this.name = name;
+    this.price = price;
+    this.count = count;
+  }
+  
+  // Save cart
+  function saveCart() {
+    sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+  }
+  -
+    // Load cart
+  function loadCart() {
+    cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+  }
+  if (sessionStorage.getItem("shoppingCart") != null) {
+    loadCart();
+  }
+  
