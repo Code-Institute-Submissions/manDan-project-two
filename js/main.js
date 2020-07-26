0a9c8e0465711c66976111b76a51b65fe3d92655
HMTRIVEDI <hirenkumar.t@yahoo.com>
2020-07-26 19:06:08 +0000

    shop product list styled and liked to js

--- js/main.js
+++ js/main.js
@@ -12,17 +12,17 @@ cartinfo.addEventListener("click", function()
 });
 })()
 
-// add item to cart function
+// add product to cart function
 
 (function(){
 
-    const cartBtn = document.querySelectorAll('.store-item-icon');
+    const cartBtn = document.querySelectorAll('.shop-product-icon');
 
     cartBtn.forEach(fuction(btn){
         btn.addEventListener('click',function(event){
            // console.log(event.target);
 
-    if(event.target.parentElement.classList.contains("store-item-icon"))
+    if(event.target.parentElement.classList.contains("shop-product-icon"))
            {
            let fullpath =
                   event.target.parentElement.previousElementSiblig.src;
@@ -30,36 +30,36 @@ cartinfo.addEventListener("click", function()
 
         let partPath = fullPath.slice(pos);
 
-        const item= {};
-        item.img = `img-csrt${partPath}`;
+        const product= {};
+        product.img = `img-csrt${partPath}`;
 
         let name = event.target.parentElement.parentElement.nextElementSiblig.children[0].children[0].textContent;
-        item.name=name;
+        product.name=name;
         let price= event.target.parentElement.parentElement.nextElementSiblig.children[0].children[1].textContent;
 
         let finalPrice = price.slice(1).trim();
 
-        item.price=finalPrice;
+        product.price=finalPrice;
 
 
 
 
 
 
-const cartItem = document.createElement('div');
+const cartproduct = document.createElement('div');
    
-       cartItem.classList(// "classlist"// );
+       cartproduct.classList(// "classlist"// );
 
-       cartItem.innerHTML = `
+       cartproduct.innerHTML = `
        
-       <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
-       <div class="item-text">
-       <p id= "cart-item-title" class="font-weight-bold mb-0"> ${item.name}</p>
+       <img src="${product.img}" class="img-fluid rounded-circle" id="product-img" alt="">
+       <div class="product-text">
+       <p id= "cart-product-title" class="font-weight-bold mb-0"> ${product.name}</p>
        <span>$</span>
-       <span id="cart-item-price" class="cart-item-price"
-       class="mb-0">${item.price}</span>
+       <span id="cart-product-price" class="cart-product-price"
+       class="mb-0">${product.price}</span>
        </div>
-       <a href="#" id='cart-item-remove' class="cart-item-remove">
+       <a href="#" id='cart-product-remove' class="cart-product-remove">
        <i class="fas fa-trash"></i>
        </a>
     
@@ -70,8 +70,8 @@ const cartItem = document.createElement('div');
        const cart= document.getElementById("cart");
        const total= document.querySelector(".cart-total-container");
 
-       cart.insertBefore(cartItem,total);
-       alert("item added to the cart");
+       cart.insertBefore(cartproduct,total);
+       alert("product added to the cart");
        showTotals();
 
             }
@@ -83,21 +83,21 @@ const cartItem = document.createElement('div');
     function showTotal (){
 
         const total = [];
-        const items = document.querySelectorAll(".cart-item-price");
+        const products = document.querySelectorAll(".cart-product-price");
 
-        items.forEach(function(item){total.push(item.textContent);
+        products.forEach(function(product){total.push(product.textContent);
         });
 
-        const totalMoney = total.reduce(function(toatl,item){
-            total += item;
+        const totalMoney = total.reduce(function(toatl,product){
+            total += product;
             return total;
         },0)
 
         const finalMoney= totalMoney.toFixed(2);
 
         document.getElementById('cart-total').textContent= finalMoney;
-        document.querySelector('.item-total').textContent= finalMoney;
-        document.getElementById('item-count').textContent= total.length;
+        document.querySelector('.product-total').textContent= finalMoney;
+        document.getElementById('product-count').textContent= total.length;
     }
 
 })();
