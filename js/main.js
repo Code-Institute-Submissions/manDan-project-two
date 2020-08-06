d14d146a5dc31637ac2a1fe47c40a9623f8e5539
HMTRIVEDI <hirenkumar.t@yahoo.com>
2020-08-06 09:49:51 +0000

    button js for cv

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@
 
 alert("You are about to see underdevlopment shoping website");
 
+$(document).ready(function () {
+    $("#cvUplode").click(function () {
+        $("Uplode").show();
+    });
+
+
+$(document).ready(function () {
+    $("#submit").click(function () {
+        $("Uplode").hide();
+    });
+
